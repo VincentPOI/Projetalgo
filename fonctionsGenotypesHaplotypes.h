ddbe54796b3b62b823a64b468c450d5fb94eaa50
hadrien <hadrien.milet@gmail.com>
2016-10-27 21:40:56 +0200

    test git w/ atom

--- fonctionsGenotypesHaplotypes.h
+++ fonctionsGenotypesHaplotypes.h
@@ -2,4 +2,3 @@
 //************************************************************************
 void generationAleatoireGeno(int taille_genotypes, int nb_max_loci_ambi, FILE* GenotypesAleatoires, FILE* GenoHaploAleatoires, int cpt_ind);
 //************************************************************************
-//test
